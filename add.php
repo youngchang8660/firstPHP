56c8aaee6399790799a69595d1d430d591b9731c
youngchang8660 <youngchang8660@gmail.com>
2020-08-04 21:33:36 -0600

    second commit

---
+++ add.php
@@ -0,0 +1,74 @@
+<?php 
+
+$errors = array('email'=>'', 'title'=>'', 'ingredients'=>'');
+$email = '';
+$title = '';
+$ingredients = '';
+
+if(isset($_POST['submit'])) {
+    // check email
+    if(empty($_POST['email'])) {
+        $errors['email'] = 'Email is required <br/>';
+    } else {
+        $email = $_POST['email'];
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors['email'] = "email must be a valid email address";
+        }
+    }
+
+    if(empty($_POST['title'])) {
+        $errors['title'] = 'Title is required <br/>';
+    } else {
+        $title = $_POST['title'];
+        // if (ctype_alpha(str_replace(' ', '', $title)) === false) {
+        //     echo 'Title must contain letters and spaces only';
+        //   }
+        // '/^[-a-zA-Z0-9 .]+$/'
+        if(!preg_match('/^[-a-zA-Z\s]+$/', $title)) {
+            $errors['title'] = 'Title must be letters and spaces only';
+        }
+    }
+    
+    if(empty($_POST['ingredients'])) {
+        $errors['ingredients'] = 'At least one ingredient is required <br/>';
+    } else {
+        $ingredients = $_POST['ingredients'];
+        if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
+            $errors['ingredients'] = 'Ingredients must be comma separated only';
+        }
+    }
+
+    if(!array_filter($errors)) {
+        header("Location: index.php");
+        exit();
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <?php include('templates/header.php') ?>
+
+    <section class="container grey-text">
+        <h4 class="center">Add a pizza</h4>
+        <form action="add.php" class="white" method="POST">
+            <label>Your Email:</label>
+            <input type="text" name="email" value="<?php echo htmlspecialchars($email) ?>">
+            <div class="red-text"><?php echo $errors['email'] ?></div>
+            <label>Pizza Title:</label>
+            <input type="text" name="title" value="<?php echo htmlspecialchars($title) ?>">
+            <div class="red-text"><?php echo $errors['title'] ?></div>
+            <label>Ingredients (comma separated):</label>
+            <input type="text" name="ingredients" value="<?php echo htmlspecialchars($ingredients) ?>">
+            <div class="red-text"><?php echo $errors['ingredients'] ?></div>
+            <div class="center">
+                <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+            </div>
+        </form>
+    </section>
+
+    <?php include('templates/footer.php') ?>
+
+</html>
--- index.php
+++ index.php
@@ -4,12 +4,8 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My first PHP file</title>
-</head>
-<body>
-    
-</body>
+
+    <?php include('templates/header.php') ?>
+    <?php include('templates/footer.php') ?>
+
 </html>
